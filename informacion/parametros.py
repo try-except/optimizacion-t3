5d18c1ac9b3f5a53b0261d3334e670e1f0944d90
Try Except <sjaramillo5@uc.cl>
2021-07-10 18:13:09 -0400

    Parámetros

--- informacion/parametros.py
+++ informacion/parametros.py
@@ -45,6 +45,8 @@ Q = list(parser.escuelas.values())
 # K: Duplas infactibles
 C = dict()
 K = dict()
+K_i = dict()
+K_ii = dict()
 for k,v in parser.areas.items():
     '''
     Genera C a partir de los costos para cada tupla (área, escuela) y cuando
@@ -54,18 +56,32 @@ for k,v in parser.areas.items():
     # Nota: cambiar de diccionario a tupla para mejor consistencia con el modelo.
     C[k] = dict()
     K[k] = list()
+    K_i[k] = list()
+    K_ii[k] = list()
     try:
         C[k][1] = parser.areas[k]['cost1']
+        if parser.areas[k]['cost1'] == 200:
+            K_i[k].append(1)
+        if 0 < parser.areas[k]['cost1'] <= 300:
+            K_ii[k].append(1)
     except KeyError as e:
         C[k][1] = 0
         K[k].append(1)
     try:
         C[k][2] = parser.areas[k]['cost2']
+        if parser.areas[k]['cost2'] == 200:
+            K_i[k].append(2)
+        if 0 < parser.areas[k]['cost2'] <= 300:
+            K_ii[k].append(2)
     except KeyError as e:
         C[k][2] = 0
         K[k].append(2)
     try:
         C[k][3] = parser.areas[k]['cost3']
+        if parser.areas[k]['cost3'] == 200:
+            K_i[k].append(3)
+        if 0 < parser.areas[k]['cost3'] <= 300:
+            K_ii[k].append(3)
     except KeyError as e:
         C[k][3] = 0
         K[k].append(3)
